0f5b0ae7036c9fc000c200fdc2da5af3f568d67e
Prashant Kumar <prashantkmr389@gmail.com>
2023-05-07 23:27:38 +0530

    lasst

---
+++ config/environment.js
@@ -0,0 +1,14 @@
+const development = {
+    name : 'development',
+    assetpath : '/assets',
+    
+}
+
+const production = {
+    name : 'production',
+}
+
+module.exports = {
+    development,
+    production
+}
